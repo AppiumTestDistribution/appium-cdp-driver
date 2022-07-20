8090f0b0866155e38e1086d4f37c29d0901357cc
saikrishna321 <saikrishna321@yahoo.com>
2022-07-20 15:31:21 +0530

    split elements

--- src/commands/element.js
+++ src/commands/element.js
@@ -1,43 +1,16 @@
 import { errors } from "@appium/base-driver";
-import { $, click } from "taiko";
-import log from "../logger";
-import { util } from "@appium/support";
-import { W3C_ELEMENT_KEY } from "@appium/base-driver/build/lib/constants";
+import { click } from "taiko";
+import { elementCache } from './find';
 
 let commands = {},
   helpers = {},
-  extensions = {},
-  elementCache = {};
-
-commands.findElOrEls = async function findElOrEls(
-  strategy,
-  selector,
-  mult,
-  context
-) {
-  log.info(`Strategy is ${strategy}`);
-  log.info(`Selector is ${selector}`);
-  log.info(`Mult is ${mult}`);
-  log.info(`Context is ${context}`);
-  if (strategy === "xpath") {
-    if (await $(selector).exists()) {
-      const elementId = util.uuidV4();
-      elementCache[elementId] = $(selector);
-      return { [W3C_ELEMENT_KEY]: elementId };
-    } else {
-      throw new errors.NoSuchAlertError(`unable to find element: ${selector}`);
-    }
-  } else if (strategy === "id") {
-    return await $(`#${selector}`).exists();
-  }
-};
+  extensions = {};
 
 async function _click(element) {
   await click(element);
 }
 commands.click = async function click(elementId) {
   const element = elementCache[elementId];
-  console.log(element);
   await _click(element);
 };
 
---
+++ src/commands/find.js
@@ -0,0 +1,37 @@
+import { errors } from "@appium/base-driver";
+import { $ } from "taiko";
+import log from "../logger";
+import { util } from "@appium/support";
+import { W3C_ELEMENT_KEY } from "@appium/base-driver/build/lib/constants";
+
+let commands = {},
+  helpers = {},
+  extensions = {},
+  elementCache = {};
+
+commands.findElOrEls = async function findElOrEls(
+  strategy,
+  selector,
+  mult,
+  context
+) {
+  log.info(`Strategy is ${strategy}`);
+  log.info(`Selector is ${selector}`);
+  log.info(`Mult is ${mult}`);
+  log.info(`Context is ${context}`);
+  if (strategy === "xpath") {
+    if (await $(selector).exists()) {
+      const elementId = util.uuidV4();
+      elementCache[elementId] = $(selector);
+      return { [W3C_ELEMENT_KEY]: elementId };
+    } else {
+      throw new errors.NoSuchAlertError(`unable to find element: ${selector}`);
+    }
+  } else if (strategy === "id") {
+    return await $(`#${selector}`).exists();
+  }
+};
+
+Object.assign(extensions, commands, helpers);
+export { commands, helpers, elementCache };
+export default extensions;
